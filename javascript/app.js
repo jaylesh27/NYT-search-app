34b70198320be627be73d2e3eb6049883da1cc7d
Brian K <karabinchak.brian@gmail.com>
2017-04-29 13:53:25 -0400

    Added function to get values on search btn click, create query url

--- javascript/app.js
+++ javascript/app.js
@@ -1,5 +1,27 @@
+var apiKey = '20ca0f5f9cc14d3d8b30f0274fbc7e91';
 
+// Search Parameters
+var queryTerm = "";
+var numResults = 0;
+var startYear = 0;
+var endYear = 0;
 
+$('#search-btn').on('click', function(){
+	queryTerm = $('#search').val();
+	numResults = $('#num-records').val();
+	startYear = $('#start-year').val();
+	endYear = $*('#end-year').val();
+});
+
+var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
+url += '?' + $.param({
+  'api-key': "20ca0f5f9cc14d3d8b30f0274fbc7e91",
+  'q': 'queryTerm'.
+  'begin_date': 'startYear',
+  'end_date': 'endYear',
+});
+
+/*
 var headline = response.docs[8].main
 
 var pubDate = response.docs[10]
@@ -12,4 +34,4 @@ var authorPerson = response.docs[15].person
 
 var autheorOriginal =  response.docs[15].original
 
-var authorOrginization =  response.docs[15].orginization 
+var authorOrginization =  response.docs[15].orginization*/
